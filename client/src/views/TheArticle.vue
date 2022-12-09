dfbee9d6d63a6a65c21d9d84dfc28d99bc2bc48e
1Louu <luc.bouvard@free.fr>
2022-12-10 00:45:00 +0100

    Update : Rework of the shop & article page

--- client/src/views/TheArticle.vue
+++ client/src/views/TheArticle.vue
@@ -1,5 +1,17 @@
 <template>
-    <h1>This is your ID : {{ items[0]._id }}</h1>
+  <div class="article" data-aos="fade-in">
+    <img alt="Placeholder Image" :src="require('@/assets/image.jpg')">
+    <div class="articledetails">
+      <h1>This is your ID : {{ items[0]._id }}</h1>
+      <p>Title : {{ items[0].name }}</p>
+      <p>Price : {{ items[0].price}} €</p>
+      <p>Manufacturer : {{ items[0].manufacturer }}</p>
+      <div>
+        <v-btn>Add to my cart</v-btn>
+        <v-btn v-on:click="deleteItem( items[0]._id)">DELETE !</v-btn>
+      </div>
+    </div>
+  </div>
 </template>
 
 
@@ -24,8 +36,42 @@ export default {
       this.error = err.message; 
     }
   }, 
+  methods: {
+    async deleteItem(id) {
+      await itemsService.deleteItem(id);
+      window.location.replace("./");
+    },
+
+  }
 }
 </script>
 
 <style scoped lang="sass">
+.article
+  background: rgba(52, 52, 52, 0.40)
+  padding: 80px
+  display: flex
+  img
+    width: 400px
+    border-radius: 20px
+  h1
+    color: white
+    font-size : 60px
+  p
+    color: white
+    font-size: 40px
+  .articledetails
+    margin: auto
+    div
+      display: flex
+      .v-btn
+        margin: auto
+        background: rgba(30, 30, 30, 0.70)
+        color: white
+        height: 50px
+        font-size: 28px
+        border-radius: 10px
+      .v-btn:first-child
+        margin-left: 0px
+        margin-right: 0px
 </style>
--- client/src/views/TheStore.vue
+++ client/src/views/TheStore.vue
@@ -1,33 +1,28 @@
 <template>
-  <div>
-    <h1>Latest Collection</h1>
-  </div>
-  <hr>
-  <div class="createItem">
+  <div  data-aos="fade-in" class="createItem">
+    <p>The store</p>
     <input type="text" id="createItem" v-model="text.name" placeholder="Type something here : ?">
     <v-btn v-on:click="createItem">Post it</v-btn>
   </div>
-  <div class="row">
+  <div class="row" data-aos="fade-in">
     <p class="error" v-if="error">{{ error }}</p>
     <div class="col-3"
       v-for="(item, index) in items"
       v-bind:item="item"
       v-bind:index="index"
       v-bind:key="item._id">
-        <p>Name : {{ item.name }}</p>
-        <p>Price : {{ item.price }}</p>
-        <p>Manufacturer : {{ item.manufacturer }}</p>
-        <div>
-        <v-btn v-on:click="deleteItem(item._id)">DELETE !</v-btn>
-        <v-btn router-link v-bind:to="'/store/'+ item._id" >Details</v-btn>
-        </div>
+        <router-link v-bind:to="'/store/'+ item._id">
+        <img alt="Placeholder Image" :src="require('@/assets/image.jpg')">
+        <p>{{ item.name }}</p>
+        <p>{{ item.price }} €</p>
+        <!-- <v-btn v-on:click="deleteItem(item._id)">DELETE !</v-btn> -->
+        </router-link>
     </div>
   </div>
 </template>
 
 <script>
 import itemsService from '@/../itemsService.js';
-
 export default {
   name: 'TheStore',
   data(){
@@ -51,12 +46,7 @@ export default {
       this.text.manufacturer = "--"; 
       await itemsService.insertItem(this.text);
       this.items = await itemsService.getItems();
-    },
-    async deleteItem(id) {
-      await itemsService.deleteItem(id);
-      this.items = await itemsService.getItems();
-    },
-
+    }
   }
 }
 </script >
@@ -71,16 +61,25 @@ export default {
   width: 400px
   padding-left: 10px
   margin-right: 20px
-
 .createItem
   margin-top: 100px
   width: 100%
   height: 50px
   background: #F89292
   display: flex
+  p
+    color: white
+    margin: auto
+    margin-left: 40px
+    margin-right: 0px
+    font-size: 28px
+
   .v-btn
     margin: auto
     margin-left: 20px
+
+a
+  text-decoration: none
 
 .row
   padding: 30px
@@ -89,15 +88,31 @@ export default {
   height: -webkit-fill-available
 
 .col-3
-  text-align: left
+  text-align: center
   color: white
-  background: rgba(216, 216, 216, 0.10)
+  background: rgba(30, 30, 30, 0.750)
   box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.20)
   margin: 30px
-  height: 200px
   padding: 20px 20px 20px 20px
   border-radius: 20px
   width: 300px
+  display: flex
+  flex-direction: column
+
+  p
+    font-size: 24px
+    color: white
+    text-decoration: none
+
+  img 
+    width: 260px
+    height: auto
+    margin-bottom: 20px
+    border-radius: 20px
+    margin-left: auto
+    margin-right: auto
+    display: block
+
   .v-btn
     background: rgba(216, 216, 216, 0.10)
     margin: auto
@@ -105,7 +120,4 @@ export default {
   
   div
     display: flex
-
-
-
 </style>
